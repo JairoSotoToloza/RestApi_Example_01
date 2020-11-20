5d1181a4d773257e447d1665a6e1aec6dcc1b7c6
JairoSotoToloza <jairosototoloza@gmail.com>
2020-11-20 14:57:55 -0300

    Se agrega en movie.js delete

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,5 +1,6 @@
-const { Router } = require('express')
+const { Router } = require('express');
 const router = Router();
+const _ = require('underscore');
 
 const movies = require('../sample.json');
 console.log(movies);
@@ -17,10 +18,20 @@ router.post('/', (req, res) => {
     movies.push(newMovie); // Guardamos la nueva pelicula en el arreglo de peliculas.
     res.json(movies); // Enviamos al cliente las peliculas actualizadas.
   } else {
-    res.send('wrong answer');
+    res.status(500).json({error: 'There was an error.'});
   } 
   
 });
 
+    router.delete('/:id', (req, res) => {
+      const { id } = req.params; // desde req.params ontenemos el id.
+      _.each(movies, (movie, i) => { // recorrer el arreglo con el metodo each obteniendo una pelicula por ciclo. 
+	if (movie.id == id) {
+	    movies.splice(i, 1); // de ser así eliminala.
+	}
+      });
+      res.send(movies); // actualiza la lista de peliculas del cliente.
+   });
 
 module.exports = router;
+
